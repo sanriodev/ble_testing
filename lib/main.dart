377d1cc0b089e75e9995da0b6e015f93eec4ef70
Matteo Juen <matteo.juen@ematric.com>
2024-10-15 15:34:33 +0200

    fix: cancel stream when creating new

--- lib/main.dart
+++ lib/main.dart
@@ -78,6 +78,18 @@ class _MyHomePageState extends State<MyHomePage> {
     _startScan();
   }
 
+  @override
+  void reassemble() {
+    _startScan();
+    super.reassemble();
+  }
+
+  @override
+  void dispose() {
+    _stopScan();
+    super.dispose();
+  }
+
   void _stopScan() async {
     await _scanStream.cancel();
   }
@@ -131,6 +143,7 @@ class _MyHomePageState extends State<MyHomePage> {
           connectionStopWatch.stop();
           connectionStopWatch.reset();
           if (retryOnFailed) {
+            currentConnectionStream.cancel();
             await Future.delayed(
               const Duration(milliseconds: 200),
             );
@@ -188,12 +201,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
